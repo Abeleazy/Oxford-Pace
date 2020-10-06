717842e6c739ad790e4d88c2f06a029f0d52f3ad
Okiemute Gold <goldokiemute@gmail.com>
2020-10-06 15:12:20 +0100

    updated services page

---
+++ js/schema.js
@@ -0,0 +1,39 @@
+    // schema
+
+  {
+     "@context": "https://schema.org/",
+     "@type": "LocalBusiness",
+     "name": "Oxford Pace Intl ltd",
+
+   <!-- "address": "27, Rajioba Street, By Engraced Events, Beside Winners Church, Egbeda, Alimosho, Lagos State", -->
+
+     "address": {
+    	"@type": "Place",
+    	"addressLocality": "Egbeda, Alimosho",
+    	"addressRegion": "Lagos State",
+    	"streetAddress": "27, Rajioba Street, By Engraced Events, Beside Winners Church"
+      },
+
+     "description": "Specializes in Real Estate Investment, Agricultural Investment, and Kiddies and Teens Investment",
+     "name": "Oxford Pace Intl Ltd",
+     "telephone": "+234(0) 800 000 0000",
+     "email": "oxfordpaceintlltd@gmail.com",
+
+     "parentOrganization": {
+	 "organization": "Oxford Group Africa",
+	 "url": "oxfordgroupafrica.com"
+      },
+
+     "logo": "",
+
+     "hasMap": {
+        "@type": "Map",
+        "mapType": {"@id": "https://schema.org/VenueMap"},
+        "url":  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15853.298338167073!2d3.293573171411124!3d6.606528085996901!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xaf6e9b2619847641!2soxford%20pace!5e0!3m2!1sen!2sng!4v1601377099916!5m2!1sen!2sng"
+      },
+
+     "geo": {
+	"lat": "",
+	"lng": ""
+     }
+  }
